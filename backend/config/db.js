fa5dd91750d6ff4a02e911fbf862bc7f455f56cd
Omkar Machindra Khodade <154883981+OmkarKhodade@users.noreply.github.com>
2024-09-07 12:15:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,14 +7,3 @@ export const connectDB = async () => {
 }
 
 
-// mongodb+srv://<username>:<1CR0L47R2Xwd9T9Q>@cluster0.jsan3.mongodb.net/
-
-// 1CR0L47R2Xwd9T9Q
-// mongodb+srv://haramistrange55:<password>@cluster0.a859u.mongodb.net/  
-
-
-
-
-
-
-// mongodb+srv://omkardada:omk12345@cluster0.eei2uzn.mongodb.net/?  
